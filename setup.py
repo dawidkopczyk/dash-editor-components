e6c2acf8327f91bba20941441537851449d64414
Dawid Kopczyk <dawid.kopczyk@gmail.com>
2019-09-15 17:04:54 +0200

    First commit

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 from setuptools import setup
 
 
-with open(os.path.join('dash_editor_components', 'package.json')) as f:
+with open(os.path.join('dash_editor_components', 'package-info.json')) as f:
     package = json.load(f)
 
 package_name = package["name"].replace(" ", "_").replace("-", "_")
